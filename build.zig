c29ba849dba6b435b60f698214f5afcc50029f8a
Leguu <33807061+Leguu@users.noreply.github.com>
2020-08-19 06:42:42 +0400

    initialize

---
+++ build.zig
@@ -0,0 +1,18 @@
+const Builder = @import("std").build.Builder;
+
+pub fn build(b: *Builder) void {
+    const mode = b.standardReleaseOptions();
+    const lib = b.addStaticLibrary("lsdl-zig", "src/main.zig");
+    lib.linkLibC();
+    lib.linkSystemLibrary("SDL2");
+    lib.setBuildMode(mode);
+    lib.install();
+
+    var main_tests = b.addTest("src/main.zig");
+    main_tests.linkLibC();
+    main_tests.linkSystemLibrary("SDL2");
+    main_tests.setBuildMode(mode);
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&main_tests.step);
+}
