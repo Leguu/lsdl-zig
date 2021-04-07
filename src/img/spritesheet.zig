d289eb8b23f7640245564fa22c890b13e2cae186
Leguu <33807061+Leguu@users.noreply.github.com>
2021-04-07 19:40:13 -0400

    Add some spritesheet functionality

--- src/img/spritesheet.zig
+++ src/img/spritesheet.zig
@@ -3,12 +3,20 @@ const lsdl = @import("../lsdl.zig");
 
 pub const Spritesheet = struct {
     image: lsdl.Image,
-    sprite_size: struct { height: u32, width: u32 },
+    sprite_size: SpriteSize,
+
+    const SpriteSize = struct { height: usize, width: usize };
 
     const Self = @This();
 
+    pub fn load(render: lsdl.Render, path: [*c]const u8, sprite_size: SpriteSize) Self {
+        return .{ .image = lsdl.Image.load(render, path), .sprite_size = sprite_size };
+    }
+
     pub fn length(self: *Self) usize {
         const size = self.image.size(usize);
-        return 0;
+        const width = size.width / self.sprite_size.width;
+        const height = size.height / self.sprite_size.height;
+        return width * height;
     }
 };
