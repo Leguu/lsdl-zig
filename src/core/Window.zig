7edc823c8c615e54d1a333e0a3d84f33d5becc67
Leguu <33807061+Leguu@users.noreply.github.com>
2021-06-02 11:53:01 -0400

    Add WindowOpts. WIP.

--- src/core/Window.zig
+++ src/core/Window.zig
@@ -5,8 +5,21 @@ window: *lsdl.SDL_Window,
 
 const Self = @This();
 
-pub fn new(s: lsdl.Size) Self {
-    const window = lsdl.SDL_CreateWindow(0, 0, 0, s.x, s.y, 0) orelse {
+pub const WindowOpts = struct {
+    title: [*c]const u8 = null,
+    flags: []Flags = &[_]Flags{},
+
+    pub const Flags = enum(u32) {
+        fullscreen = lsdl.SDL_WINDOW_FULLSCREEN,
+    };
+};
+
+pub fn new(s: lsdl.Size, opts: anytype) Self {
+    var result: u32 = 0;
+    for (opts.flags) |flag| {
+        result = @enumToInt(flag) | result;
+    }
+    const window = lsdl.SDL_CreateWindow(opts.title, 0, 0, s.x, s.y, 0) orelse {
         std.debug.panic("Error has occured creating window: {}\n", .{lsdl.SDL_GetError().*});
     };
 
